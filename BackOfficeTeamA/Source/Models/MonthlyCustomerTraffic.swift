de31dd65c124a8f83a5c86c37609b0ace9a1976b
jwoo820 <pjwoo820@gmail.com>
2022-12-27 15:16:24 +0900

    [Feat] 월별 고객 트래픽 구조체 생성

--- BackOfficeTeamA/Source/Models/MonthlyCustomerTraffic.swift
+++ BackOfficeTeamA/Source/Models/MonthlyCustomerTraffic.swift
@@ -7,4 +7,15 @@
 
 import Foundation
 
+struct MonthlyCustomerTraffic: Identifiable {
+    var id = UUID().uuidString
 
+    var date: Date
+    var customerTraffic: Double
+    
+    init(month: Int, customerTraffic: Double) {
+        let calendar = Calendar.autoupdatingCurrent
+        self.date = calendar.date(from: DateComponents(year: 2022, month: month))!
+        self.customerTraffic = customerTraffic
+    }
+}
