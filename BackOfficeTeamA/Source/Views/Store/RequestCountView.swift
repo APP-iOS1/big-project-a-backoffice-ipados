bf52eb9d11933f352081e5dc1a312c2d92e1377d
Heodoo <39407635+Heodoo@users.noreply.github.com>
2022-12-27 17:04:02 +0900

    [Feat] RequestCountView UI

---
+++ BackOfficeTeamA/Source/Views/Store/RequestCountView.swift
@@ -0,0 +1,30 @@
+//
+//  EnrollRequestCountView.swift
+//  BackOfficeTeamA
+//
+//  Created by MacBook on 2022/12/27.
+//
+
+import SwiftUI
+
+struct RequestCountView: View {
+    var body: some View {
+        VStack(alignment: .leading){
+            Text("New / Enroll Request")
+                .font(.headline)
+            Spacer()
+            HStack{
+                Text("3").foregroundColor(.secondary)
+                Text("/ 7")
+            }
+            .font(.title)
+            
+        }.modifier(TopItemModifier())
+    }
+}
+
+struct RequestCountView_Previews: PreviewProvider {
+    static var previews: some View {
+        RequestCountView().frame(height: 100)
+    }
+}
