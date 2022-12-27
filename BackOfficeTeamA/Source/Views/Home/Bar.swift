676da2da6318fa81d6887aafa275b38937f2481a
jwoo820 <pjwoo820@gmail.com>
2022-12-27 14:12:08 +0900

    [Add] Bar 차트 파일 생성

---
+++ BackOfficeTeamA/Source/Views/Home/Bar.swift
@@ -0,0 +1,20 @@
+//
+//  Bar.swift
+//  BackOfficeTeamA
+//
+//  Created by Park Jungwoo on 2022/12/27.
+//
+
+import SwiftUI
+
+struct Bar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Bar_Previews: PreviewProvider {
+    static var previews: some View {
+        Bar()
+    }
+}
