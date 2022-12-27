01bcaead49b5b5b6720b45b0860ee4429b6f93bf
jwoo820 <pjwoo820@gmail.com>
2022-12-27 14:11:11 +0900

    [Add] Pie차트 파일 생성

---
+++ BackOfficeTeamA/Source/Views/Home/Pie.swift
@@ -0,0 +1,20 @@
+//
+//  Pie.swift
+//  BackOfficeTeamA
+//
+//  Created by Park Jungwoo on 2022/12/27.
+//
+
+import SwiftUI
+
+struct Pie: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Pie_Previews: PreviewProvider {
+    static var previews: some View {
+        Pie()
+    }
+}
