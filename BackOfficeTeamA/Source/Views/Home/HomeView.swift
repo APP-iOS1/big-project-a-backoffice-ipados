c9b045f58e3ea242489327c0a94daf771dcfcf16
jwoo820 <pjwoo820@gmail.com>
2022-12-27 15:17:47 +0900

    [Feat] 차트 컴포넌트 배치

--- BackOfficeTeamA/Source/Views/Home/HomeView.swift
+++ BackOfficeTeamA/Source/Views/Home/HomeView.swift
@@ -5,11 +5,42 @@
 //  Created by MacBook on 2022/12/27.
 //
 
+import Charts
 import SwiftUI
 
 struct HomeView: View {
+    
     var body: some View {
-        Text("HomeView")
+        ScrollView {
+            VStack {
+                HStack {
+                    Bar()
+                    Pie(slices: [
+                        (2, .red),
+                        (3, .orange),
+                        (4, .yellow),
+                        (1, .green),
+                        (5, .blue),
+                        (4, .indigo),
+                        (2, .purple)
+                    ])
+                }
+                HStack {
+                    LineChart()
+                    Pie(slices: [
+                        (2, .red),
+                        (3, .orange),
+                        (4, .yellow),
+                        (1, .green),
+                        (5, .blue),
+                        (4, .indigo),
+                        (2, .purple)
+                    ])
+                }
+            }
+            .padding()
+        }
+        .navigationTitle("Dashboard")
     }
 }
 
