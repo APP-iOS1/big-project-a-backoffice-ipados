fcc26dd7031e1b851d55312acc26a827eccb9686
jwoo820 <pjwoo820@gmail.com>
2023-01-03 11:11:36 +0900

    [Feat] Notification 모델 수정

--- BackOfficeTeamA/Source/Models/Notification.swift
+++ BackOfficeTeamA/Source/Models/Notification.swift
@@ -6,8 +6,9 @@
 //
 
 import Foundation
+import SwiftUI
 
-struct Notification {
+struct Notification: Identifiable, Hashable {
     var id = UUID().uuidString
     var receiverId: String
     var isViewed: Bool
@@ -15,14 +16,18 @@ struct Notification {
     var notificationDescription: String
     var notificationDate: Date
     var appCategory: String
+    
+    var notifiactionDateAt: String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        return dateFormatter.string(from: notificationDate)
+    }
+    
+    static var sample: [Notification] {
+        Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Store1", notificationDescription: "테스트 메시지1입니다.", notificationDate: Date(), appCategory: "store")
+        Notification(receiverId: UUID().uuidString, isViewed: false, notificationTitle: "Store2", notificationDescription: "테스트 메시지2입니다.", notificationDate: Date(), appCategory: "store")
+        Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Customer1", notificationDescription: "테스트 메시지3입니다.", notificationDate: Date(), appCategory: "customer")
+        Notification(receiverId: UUID().uuidString, isViewed: false, notificationTitle: "Customer2", notificationDescription: "테스트 메시지4입니다.", notificationDate: Date(), appCategory: "customer")
+        Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Backoffice", notificationDescription: "테스트 메시지5입니다.", notificationDate: Date(), appCategory: "backoffice")
+    }
 }
-
-let dummyNoti = [
-    Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Store1", notificationDescription: "메세지1입니다.", notificationDate: Date(), appCategory: "store")
-    Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Store2", notificationDescription: "메세지2입니다.", notificationDate: Date(), appCategory: "store")
-    Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Customer1", notificationDescription: "메세지3입니다.", notificationDate: Date(), appCategory: "customer")
-    Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Customer2", notificationDescription: "메세지4입니다.", notificationDate: Date(), appCategory: "customer")
-    Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Noti1", notificationDescription: "메세지5입니다.", notificationDate: Date(), appCategory: "backoffice")
-    Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Noti1", notificationDescription: "메세지6입니다.", notificationDate: Date(), appCategory: "backoffice")
-    Notification(receiverId: UUID().uuidString, isViewed: true, notificationTitle: "Noti1", notificationDescription: "메세지7입니다.", notificationDate: Date(), appCategory: "backoffice")
-]
