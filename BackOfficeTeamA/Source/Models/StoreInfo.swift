858836b83c7955cdc2ae7f9e6e68a9f4e70b2e46
Heodoo <39407635+Heodoo@users.noreply.github.com>
2023-01-03 18:57:02 +0900

    [Feat] StoreInfo 모델 수정 , 연산프로퍼티 추가

--- BackOfficeTeamA/Source/Models/StoreInfo.swift
+++ BackOfficeTeamA/Source/Models/StoreInfo.swift
@@ -7,20 +7,37 @@
 
 import Foundation
 
-struct StoreInfo: Identifiable {
-    var id: String
-    var storeName: String
-    var storeEmail: String
-    var storeLocation: String
-    var registerDate: Date
-    // 누적신고
-    var reportingCount: Int
-    var storeImage: String?
-    var phoneNumber: String
-    // 입점 허가 여부
-    var isVerified: Bool = false
-    // 입점 신청 여부
-    var isSubmitted: Bool = false
-    // 경고 여부
-    var isBanned: Bool = false
+struct StoreInfo: Identifiable,Hashable {
+        var id: String // id: UUID().uuidString
+        var storeName: String
+        var storeEmail: String
+        var storeLocation: String
+        var registerDate: Date
+        // 누적신고
+        var reportingCount: Int
+        var storeImage: String?
+        var phoneNumber: String
+        // 입점 허가 여부
+        var isVerified: Bool = false
+        // 입점 신청 여부
+        var isSubmitted: Bool = false
+        // 경고 여부
+        var isBanned: Bool = false
+        
+        // MARK: - 연산 프로퍼티
+        var isVerifiedInt : Int {
+            isVerified ? 0 : 1
+        }
+        var isVerifiedString : String {
+            isVerified ? "0" : "1"
+        }
+        var isBannedInt : Int {
+            isBanned ? 0 : 1
+        }
+    
+        var registerDateAt : String {
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "YYYY-MM-dd"
+            return dateFormatter.string(from: registerDate)
+        }
 }
