e62bddb20564d61d3fb4e82354155be15393bdab
jwoo820 <pjwoo820@gmail.com>
2023-01-03 11:42:51 +0900

    [Feat] Test Cell UI 구현

--- BackOfficeTeamA/Source/Views/Notification/NotificationView.swift
+++ BackOfficeTeamA/Source/Views/Notification/NotificationView.swift
@@ -8,13 +8,27 @@
 import SwiftUI
 
 struct NotificationView: View {
+    @State private var pickerSelection = 0
+    @State private var data = Notification.sample
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Table(data) {
+                TableColumn("이름", value: \.receiverName)
+                TableColumn("메세지 제목", value: \.notificationTitle)
+                TableColumn("앱 종류", value: \.appCategory)
+                
+                TableColumn("수신 여부") { item in
+                    Image(systemName: item.isViewed ? "checkmark" : "xmark")
+                        .foregroundColor(item.isViewed ? Color.green : Color.red)
+                }
+            }
+        }.navigationTitle("앱 알림 관리")
     }
 }
 
 struct NotificationView_Previews: PreviewProvider {
     static var previews: some View {
-        NotificationView()
+        NotificationView().previewInterfaceOrientation(.landscapeRight)
     }
 }
