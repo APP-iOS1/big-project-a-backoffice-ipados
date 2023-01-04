4aa2ef5c4677d0a895ac93fc6b78a3ca83bffc4a
jwoo820 <pjwoo820@gmail.com>
2023-01-04 13:56:42 +0900

    [Add] PaymentDetailView 추가

---
+++ BackOfficeTeamA/Source/Views/Payment/PaymentDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  PaymentDetailView.swift
+//  BackOfficeTeamA
+//
+//  Created by Park Jungwoo on 2023/01/04.
+//
+
+import SwiftUI
+
+struct PaymentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PaymentDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        PaymentDetailView()
+    }
+}
